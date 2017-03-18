4ab880cfff0f5eb4e34a90811da48b05dc8a36ae
jajab <jaber.jade@gmail.com>
2017-03-18 11:24:07 +0100

    ajout CV

---
+++ operating/copy_CV.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+cp "/home/jajab/Dropbox/Perso/Recherche emploi/CV - Jade Jaber - EN.pdf" /home/jajab/Developpement/_sources/MonSite/jadejaber.github.io/documents/
+cp "/home/jajab/Dropbox/Perso/Recherche emploi/CV - Jade Jaber - FR.pdf" /home/jajab/Developpement/_sources/MonSite/jadejaber.github.io/documents/
---
+++ operating/deploy_local.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+bundle exec jekyll serve --config  /home/jajab/Developpement/_sources/MonSite/jadejaber.github.io/_config_local.yml
