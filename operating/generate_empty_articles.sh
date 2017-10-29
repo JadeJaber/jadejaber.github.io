b374745413a79202fe86302273315bb615931cda
Jade JABER <jaber.jade@gmail.com>
2017-10-29 17:27:41 +0100

    new articles

--- operating/generate_empty_articles.sh
+++ operating/generate_empty_articles.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-for i  in 1 2 3 4 5 6 7 8 9 
+for i  in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
 do
 	cp ./metadata_jekyll.txt  ./_posts/articles/2017-01-01-empty-$i.md	
 done
